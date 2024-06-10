1f9b27b9ac256156b86777094a1e92cd7128ccad
cuong <cuongmac2k@gmail.com>
2024-06-10 17:28:38 +0700

    abc

--- nhap/getResource/main.go
+++ nhap/getResource/main.go
@@ -9,21 +9,22 @@ import (
 	"github.com/google/uuid"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 	"time"
 )
 
 func main() {
 	a := time.Now()
-	//b := getValueResouce("https://staging.bizflycloud.vn/api/iam/v2/projects?limit=100&sort=created_at:asc", "Get")
+	b := getValueResouce("https://hn-staging2.bizflycloud.vn/api/iaas-cloud/firewalls/2a0ce5b9-b05b-4741-a547-68eaa8d738dd",
+		"PATCH", "98f5e866-ce84-41d8-b09c-d883a86e96c0")
 	//b := getValueResouce("https://hn-staging2.bizflycloud.vn/api/kubernetes-engine/_/b8sm4tbo4lg2frb2", "Get")
-	b := getValueResouce("https://staging.bizflycloud.vn/api/al/visualize/explore/b0120bb8-ae50-4cbd-aab7-65835ec0fbd2?schemaVersion=1&panes=%20%20%20%20%20%20%7B%22mig%22:%7B%22datasource%22:%22f9301251-dd7e-4509-84b4-b1e2809e52b7%22,%22queries%22:%5B%7B%22refId%22:%22A%22,%22expr%22:%22%7B%20%20%20%20%20%20project_uuid%3D%5C%22b0120bb8-ae50-4cbd-aab7-65835ec0fbd2%5C%22%7D%20%7C%3D%20%60%60%22,%22queryType%22:%22range%22,%22datasource%22:%7B%22type%22:%22loki%22,%22uid%22:%22f9301251-dd7e-4509-84b4-b1e2809e52b7%22%7D,%22editorMode%22:%22builder%22%7D%5D,%22range%22:%7B%22from%22:%22now-1h%22,%22to%22:%22now%22%7D%7D%7D&orgId=1", "Get")
+	//b := getValueResouce("https://staging.bizflycloud.vn/api/al/visualize/explore/b0120bb8-ae50-4cbd-aab7-65835ec0fbd2?schemaVersion=1&panes=%20%20%20%20%20%20%7B%22mig%22:%7B%22datasource%22:%22f9301251-dd7e-4509-84b4-b1e2809e52b7%22,%22queries%22:%5B%7B%22refId%22:%22A%22,%22expr%22:%22%7B%20%20%20%20%20%20project_uuid%3D%5C%22b0120bb8-ae50-4cbd-aab7-65835ec0fbd2%5C%22%7D%20%7C%3D%20%60%60%22,%22queryType%22:%22range%22,%22datasource%22:%7B%22type%22:%22loki%22,%22uid%22:%22f9301251-dd7e-4509-84b4-b1e2809e52b7%22%7D,%22editorMode%22:%22builder%22%7D%5D,%22range%22:%7B%22from%22:%22now-1h%22,%22to%22:%22now%22%7D%7D%7D&orgId=1", "Get")
 	fmt.Println(b)
-	//fmt.Println(getValueProject("b0120bb8-ae50-4cbd-aab7-65835ec0fbd2"))
 	fmt.Println(time.Since(a))
 }
-func getValueResouce(resource string, method string) string {
-	url := fmt.Sprintf("%s/admin/resources", "https://staging.bizflycloud.vn/api/iam/v2")
+func getValueResouce(resource string, method, service string) string {
+	url := fmt.Sprintf("%s/admin/resources?%s%s", "https://staging.bizflycloud.vn/api/iam/v2", "service_uuid=", service)
 	body, err := CallAPI("GET", url, nil)
 	if err != nil {
 		fmt.Println("err 1", err)
@@ -41,21 +42,43 @@ func getValueResouce(resource string, method string) string {
 		fmt.Println("err 2 ", err)
 	}
 	for _, v := range res1 {
-		//fmt.Println(" data", v)
+		if strings.ToUpper(v.Type) == strings.ToUpper(method) {
+			if len(v.Patterns) > 0 {
+				s := resource
+				for _, i := range v.Patterns {
+					re := regexp.MustCompile(i)
 
-		if strings.ToUpper(v.Type) == strings.ToUpper(method) && strings.Contains(resource, v.Url) {
-			fmt.Println("url ", v.Url)
-			fmt.Println("record ", v)
+					// Thay thế tất cả các chuỗi khớp với regex bằng dấu *
+					result := re.ReplaceAllString(s, "*")
+					//fmt.Println("reslut sau khi thay *: ", result)
+					//
+					if strings.Contains(result, v.Url) {
+						//return v.Endpoint
+						fmt.Println("url ", v.Url)
+						fmt.Println("enpoint ", v.Endpoint)
+						//fmt.Println("record ", v)
+						break
+					}
+				}
+
+			}
+			if strings.Contains(resource, v.Url) {
+				//return v.Endpoint
+				fmt.Println(" out url ", v.Url)
+				fmt.Println("out record ", v)
+			}
+
 		}
 	}
 	return ""
 }
 
 type dataResource struct {
-	Type        string `json:"type"`
-	Endpoint    string `json:"endpoint"`
-	Description string `json:"description"`
-	Url         string `json:"url"`
+	Patterns    []string `json:"patterns"`
+	Type        string   `json:"type"`
+	Endpoint    string   `json:"endpoint"`
+	Description string   `json:"description"`
+	Url         string   `json:"url"`
 }
 
 func CallAPI(method, url string, bodyJSON []byte) ([]byte, error) {
--- nhap/regex/main.go
+++ nhap/regex/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/google/uuid"
+	"regexp"
 )
 
 func main() {
-	fmt.Println(uuid.New())
+	// Định nghĩa regex pattern
+	pattern := `[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}`
+
+	// Chuỗi mẫu để kiểm tra
+	text := "Example UUID: 2a0ce5b9-b05b-4741-a547-68eaa8d738dd and another UUID: b0120bb8-ae50-4cbd-aab7-65835ec0fbd2 b0120bb8-ae50-4cbd-aab7-65835ec0fbd2123e4567-e89b-12d3-a456-426614174000."
+
+	// Compile regex
+	re := regexp.MustCompile(pattern)
+
+	// Thay thế tất cả các chuỗi khớp với regex bằng dấu *
+	result := re.ReplaceAllString(text, "*")
+
+	// In kết quả
+	fmt.Println(result)
 }
